ff5aeed83820f27a96333ed84148fc78d81a695a
jaked122 <jaked122@gmail.com>
2015-12-20 12:49:33 -0500

    Removed the center square being set

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ int main() {
   sf::RenderWindow r(sf::VideoMode(520, 256), "Symmetricity");
   Agony e;
   sf::View q;
-  q.setSize(520,256);
-  q.setCenter(0,0);
+  q.setSize(520, 256);
+  q.setCenter(0, 0);
   r.setView(q);
-  e.set_thing(0, 0, 0);
   /*e.set_thing(1, 1, 0);
   e.set_thing(1, 2, 0);
   e.set_thing(1, 1, -1);
@@ -24,44 +23,44 @@ int main() {
     while (r.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         r.close();
-      if(event.type==sf::Event::Resized){
-        q.setSize(r.getSize().x,r.getSize().y);
+      if (event.type == sf::Event::Resized) {
+        q.setSize(r.getSize().x, r.getSize().y);
         r.setView(q);
       }
-      if(event.type==sf::Event::MouseMoved){
-        sf::Vector2i g=sf::Mouse::getPosition(r);
-        auto q=r.mapPixelToCoords(g);
-        q.x/=10;
-        q.y/=10;
-        e.mouse_over(q);
+      if (event.type == sf::Event::MouseMoved) {
+        sf::Vector2i g = sf::Mouse::getPosition(r);
+        auto q = r.mapPixelToCoords(g);
+        q.x /= 10;
+        q.y /= 10;
+        e.mouse_over(q);//tells the thing to display a circle where the mouse is over.
       }
-      if(event.type==sf::Event::MouseButtonPressed){
-        sf::Vector2i g=sf::Mouse::getPosition(r);
-        auto q=r.mapPixelToCoords(g);
-        q.x/=10;
-        q.y/=10;
+      if (event.type == sf::Event::MouseButtonPressed) {
+        sf::Vector2i g = sf::Mouse::getPosition(r);
+        auto q = r.mapPixelToCoords(g);
+        q.x /= 10;
+        q.y /= 10;
         e.long_desig(q);
       }
       if (event.type == sf::Event::KeyPressed) {
-        int times=1;
-        if(event.key.shift)
-          times=10;
+        int times = 1;
+        if (event.key.shift)
+          times = 10;
         switch (event.key.code) {
         case sf::Keyboard::Left:
-          for(int i=0;i<times;i++)
-          e.increment_cursor_x();
+          for (int i = 0; i < times; i++)
+            e.increment_cursor_x();
           break;
         case sf::Keyboard::Right:
-          for(int i=0;i<times;i++)
-          e.decrement_cursor_x();
+          for (int i = 0; i < times; i++)
+            e.decrement_cursor_x();
           break;
         case sf::Keyboard::Down:
-          for(int i=0;i<times;i++)
-          e.decrement_cursor_y();
+          for (int i = 0; i < times; i++)
+            e.decrement_cursor_y();
           break;
         case sf::Keyboard::Up:
-          for(int i=0;i<times;i++)
-          e.increment_cursor_y();
+          for (int i = 0; i < times; i++)
+            e.increment_cursor_y();
           break;
         case sf::Keyboard::Period:
           e.increase_z();
@@ -76,11 +75,11 @@ int main() {
           e.add_y_symmetry_at_cursor();
           break;
         case sf::Keyboard::Subtract:
-          case sf::Keyboard::Dash:
+        case sf::Keyboard::Dash:
           e.decrease_activity();
           break;
         case sf::Keyboard::Add:
-          case sf::Keyboard::Equal:
+        case sf::Keyboard::Equal:
           e.increase_activity();
           break;
         case sf::Keyboard::Space:
@@ -89,7 +88,7 @@ int main() {
         case sf::Keyboard::Return:
           e.long_desig();
           break;
-        case sf::Keyboard::F5://TODO Write a method to select a file to write to.
+        case sf::Keyboard::F5: //TODO Write a method to select a file to write to.
           e.write_file_output("output.csv");
         }
       }
