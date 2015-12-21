9727a85dc9f21419f1450eb973d3ca009c218117
jaked122 <jaked122@gmail.com>
2015-12-21 10:58:28 -0500

    Planning to add support for proper rotation

--- TransformationMatrix.hpp
+++ TransformationMatrix.hpp
@@ -1,5 +1,6 @@
-#ifndef REFLECTIONMATRIX_HPP
-#define RELFECTIONMATRIX_HPP
+#ifndef TRANSFORMATIONMATRIX_HPP
+#define TRANSFORMATIONMATRIX_HPP
 #include <Eigen/Dense>
 Eigen::Matrix3d make_Reflection(int x,int y,int z);
+Eigen::Matrix3d make_Rotation(float degree1, float degree2);
 #endif
